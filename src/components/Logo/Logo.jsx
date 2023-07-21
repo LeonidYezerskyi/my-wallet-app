f6ad1580b95b170899b84ea1813fc4c815334168
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2023-07-21 18:18:48 +0300

    ADDED: content and stylies for Logo component

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,12 @@
 import css from "./logo.module.css"
+import wallet from "../../assets/wallet.svg"
 
 const Logo = () => {
     return (
-        <div>Logo</div>
+        <div className={css.logoWrapper}>
+            <p className={css.logoText}>CryptoWALLET</p>
+            <img className={css.logoImg} src={wallet} alt="logo" />
+        </div>
     )
 }
 
